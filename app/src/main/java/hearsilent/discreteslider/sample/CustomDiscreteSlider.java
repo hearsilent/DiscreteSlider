d360c9f15872c1aa2969ab53ec3b1da5a533e65f
HearSilent <hear.silent1995@gmail.com>
2019-05-03 12:28:48 +0800

    Update version to 1.0.8, Code tune

--- app/src/main/java/hearsilent/discreteslider/sample/CustomDiscreteSlider.java
+++ app/src/main/java/hearsilent/discreteslider/sample/CustomDiscreteSlider.java
@@ -9,17 +9,6 @@ import hearsilent.discreteslider.DiscreteSlider;
 
 public class CustomDiscreteSlider extends DiscreteSlider {
 
-	@Override
-	public void onDrawThumb(Canvas canvas, float cx, float cy, boolean hasTouched) {
-		// Draw thumb you want
-	}
-
-	// Override this value to your thumb size
-	@Override
-	public int getSize() {
-		return super.getSize();
-	}
-
 	public CustomDiscreteSlider(Context context) {
 		super(context);
 	}
@@ -30,5 +19,16 @@ public class CustomDiscreteSlider extends DiscreteSlider {
 
 	public CustomDiscreteSlider(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+	}
+
+	@Override
+	public void onDrawThumb(Canvas canvas, float cx, float cy, boolean hasTouched) {
+		// Draw thumb you want
+	}
+
+	@Override
+	public int getSize() {
+		// Override this value to your thumb size
+		return super.getSize();
 	}
 }
