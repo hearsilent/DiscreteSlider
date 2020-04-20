48e0ab65e7392dc0bf0a5f94cfc54388d61ba5fc
HearSilent <hear.silent1995@gmail.com>
2020-04-20 12:12:17 +0800

    Fix sometimes value lable disappear

--- discreteslider/src/main/java/hearsilent/discreteslider/DiscreteSlider.java
+++ discreteslider/src/main/java/hearsilent/discreteslider/DiscreteSlider.java
@@ -689,6 +689,7 @@ public class DiscreteSlider extends View {
 					mValueLabelAnimator = null;
 				}
 				mValueLabelAnimValue = 0;
+				mValueLabelIsShowing = false;
 				mShowValueLabelHandler.removeCallbacksAndMessages(null);
 			}
 
